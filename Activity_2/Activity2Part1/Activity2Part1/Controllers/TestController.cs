3f8879067236ac2ea3a32f469eeda6c07b3fd4aa
Nick_Thomas <Nick_thomas@github.com>
2020-11-22 21:37:23 -0700

    activity_2

---
+++ Activity_2/Activity2Part1/Activity2Part1/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using Activity2Part1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Activity2Part1.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            List<UserModel> userList = new List<UserModel>();
+            UserModel user1 = new UserModel("Nick", "Nicholas@email.com", "123-456-7890");
+            UserModel user2 = new UserModel("Ashley", "Ashley@email.com", "123-456-7890");
+            UserModel user3 = new UserModel("Zane", "Zane@email.com", "123-456-7890");
+
+            userList.Add(user1);
+            userList.Add(user2);
+            userList.Add(user3);
+
+            return View("Test", userList);
+            // return View(new UserModel());
+        }
+    }
+}
---
+++ Activity_2/Activity2Part1/Activity2Part1/Models/UserModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Activity2Part1.Models
+{
+    public class UserModel
+    {
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+
+
+        public UserModel()
+        {
+            // Do Nothing
+        }
+
+
+        public UserModel(string name, string email, string phone)
+        {
+            this.Name = name;
+            this.EmailAddress = email;
+            this.PhoneNumber = phone;
+        }
+
+    }
+}
+    
